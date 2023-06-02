dcc8c411747aabb7e672e0536ee184ec03c386b4
PlanetofJay <jae44dv@gmail.com>
2023-06-02 18:46:28 -0400

    test both players

--- playground-dice/dice.js
+++ playground-dice/dice.js
@@ -1,10 +1,17 @@
-// random number between 1 and 6
-let randomNum1 = Math.floor(Math.random() * 6) + 1;
+// Player 1
+let randomNum1 = Math.floor(Math.random() * 6) + 1; // random number between 1 and 6
 
-// dice1.png to dice6.png
-let randomDiceImg = "dice" + randomNum1 + ".png";
+let randomDiceImg = "dice" + randomNum1 + ".png"; // dice1.png to dice6.png
 
-// images/dice1.png to images/dice6.png
-let randomImgSrc = "images/" + randomDiceImg;
+let randomImgSrc = "images/" + randomDiceImg; // images/dice1.png to images/dice6.png
 
-document.querySelectorAll("img")[0].setAttribute("src", randomImgSrc);
+let player1 = document.querySelectorAll("img")[0];
+
+player1.setAttribute("src", randomImgSrc);
+
+// Player 2
+let randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImgSrc2 = "images/dice" + randomNum2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
