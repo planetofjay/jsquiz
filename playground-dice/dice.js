b24e45407b3a3e51d5dca041f27a01d42a7de995
PlanetofJay <jae44dv@gmail.com>
2023-06-02 18:37:47 -0400

    test player1

--- playground-dice/dice.js
+++ playground-dice/dice.js
@@ -1,4 +1,10 @@
-// variable with is a random number between 1 and 6
-let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+// random number between 1 and 6
+let randomNum1 = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNumber1);
+// dice1.png to dice6.png
+let randomDiceImg = "dice" + randomNum1 + ".png";
+
+// images/dice1.png to images/dice6.png
+let randomImgSrc = "images/" + randomDiceImg;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImgSrc);
