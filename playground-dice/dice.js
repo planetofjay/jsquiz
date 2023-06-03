72862eceb2890727599aea96dd68be333851f5f6
PlanetofJay <jae44dv@gmail.com>
2023-06-02 23:09:58 -0400

    test match

--- playground-dice/dice.js
+++ playground-dice/dice.js
@@ -15,3 +15,12 @@ let randomNum2 = Math.floor(Math.random() * 6) + 1;
 let randomImgSrc2 = "images/dice" + randomNum2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
+
+// if Player 1 wins
+if (randomNum1 > randomNum2) {
+  document.querySelector("h1").innerHTML = "🤙 Player 1 Wins!";
+} else if (randomNum2 > randomNum1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🤙"
+} else {
+  document.querySelector("h1").innerHTML = "🤷Draw!🤷‍♂️";
+}
