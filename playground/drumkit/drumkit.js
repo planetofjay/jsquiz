1076a723a0c97b66211d36bacf7ac3325c83d300
PlanetofJay <jae44dv@gmail.com>
2023-06-03 02:51:04 -0400

    add css, audio

--- playground/drumkit/drumkit.js
+++ playground/drumkit/drumkit.js
@@ -3,7 +3,10 @@ let numberOfDrumBtn = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDrumBtn; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+
+    let audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();
+
   });
 
 }
