974a320020c5cc6a2df93fd61f41bdb997f230d6
PlanetofJay <jae44dv@gmail.com>
2023-06-04 23:35:59 -0400

    test effects

--- playground/jQuery/jQuery.js
+++ playground/jQuery/jQuery.js
@@ -32,10 +32,30 @@
 //   console.log(event.key);
 // });
 
-$(document).keydown(function(event) {
-  $("h1").text(event.key);
-});
+// $(document).keydown(function(event) {
+//   $("h1").text(event.key);
+// });
 
-$("h1").on("mouseover", function() {
-  $("h1").css("color", "purple");
-})
+// $("h1").on("mouseover", function() {
+//   $("h1").css("color", "purple");
+// });
+
+// $("h1").before("<button>before</button>");
+// $("h1").after("<button>after</button>");
+// $("h1").prepend("<button>prepend</button>");
+// $("h1").append("<button>append</button>");
+
+// $("button").remove();
+
+$("button").on("click", function() {
+  // $("h1").hide();
+  // $("h1").toggle();
+  // $("h1").fadeOut();
+  // $("h1").fadeIn();
+  // $("h1").fadeToggle();
+  // $("h1").slideUp();
+  // $("h1").slideDown();  
+  // $("h1").slideToggle();
+  // $("h1").animate({opacity: 0.5});
+  $("h1").slideUp().slideDown().animate({opacity: 0.5})
+});
