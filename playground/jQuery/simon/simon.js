41d752d0a7889b23f5cf03f500a887ddad4faaa3
PlanetofJay <jae44dv@gmail.com>
2023-06-05 17:30:30 -0400

    test checkAnswer()

--- playground/jQuery/simon/simon.js
+++ playground/jQuery/simon/simon.js
@@ -3,16 +3,50 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+let started = false;
+let level = 0;
+
+$(document).on("keypress", function() {
+  if(!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").on("click", function() {
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   
   playSound(userChosenColour);
-  
+
   animatePress(userChosenColour);
-})
+
+  // passing in the index of last answer in the user's sequence.
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+  } else if (userClickedPattern.length === gamePattern.length) {
+    setTimeout(() => {
+      nextSequence
+    }, 1000);
+  } else {
+    console.log("fail");
+  }
+}
 
 function nextSequence() {
+  // when it's triggered, reset the userClickedPattern to an empty array ready for the next level.
+  userClickedPattern = [];
+
+  level++;
+
+  $("#level-title").text("Level " + level);
+
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
 
@@ -31,7 +65,7 @@ function playSound(name) {
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
 
-  setTimeout(function() {
+  setTimeout(() => {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
 }
