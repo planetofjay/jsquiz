fafd4903f9d9d552c6ce3af13ba2ed832cc6d5f3
PlanetofJay <jae44dv@gmail.com>
2023-06-04 23:00:11 -0400

    test

---
+++ playground/jQuery/jQuery.js
@@ -0,0 +1,9 @@
+
+// $("h1").addClass("big-title big-margin");
+
+// $("h1").removeClass("big-title");
+// $("h1").hasClass("big-margin"); // true
+
+$("h1").text("Good Night")
+
+$("button").html("<em>Hi</em>");
