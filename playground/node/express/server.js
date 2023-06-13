341602cf7fa824882f65f0ddf862bdc3f091c0c7
PlanetofJay <jae44dv@gmail.com>
2023-06-12 21:14:28 -0400

    test

--- playground/node/express/server.js
+++ playground/node/express/server.js
@@ -4,6 +4,16 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000, function() {
+app.get("/", function(request, response){
+  // console.log(request);
+  // console.log(response);
+  response.send("<h1>Planet of Jay</h1>");
+});
+
+app.get("/contact", function(request, response){
+  response.send("Contact me at: jae44dv@gmail.com");
+});
+
+app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
