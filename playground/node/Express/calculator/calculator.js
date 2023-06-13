436e5cfe10ed198379fc62d8f129c9c024078fe5
PlanetofJay <jae44dv@gmail.com>
2023-06-13 17:11:56 -0400

    update

--- playground/node/Express/calculator/calculator.js
+++ playground/node/Express/calculator/calculator.js
@@ -1,3 +1,11 @@
+/**
+ * From the Node.js documentation:
+ * No require, exports, module.exports, __filename, __dirname
+ * These CommonJS variables are not available in ES modules.
+ * require can be imported into an ES module using module.createRequire().
+ * Equivalents of __filename and __dirname can be created inside of each file via import.meta.url:
+ */
+
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
@@ -7,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.get("/", function(req,res){
-  console.log(__dirname);
+  res.sendFile(__dirname + "/calculator.html");
 });
 
 app.listen(3000, function(req,res){
