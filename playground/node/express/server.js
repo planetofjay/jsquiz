84a1fde86cf05849670a404a7fc5b872c627a666
PlanetofJay <jae44dv@gmail.com>
2023-06-12 21:22:32 -0400

    test routes, nodemon

--- playground/node/express/server.js
+++ playground/node/express/server.js
@@ -4,14 +4,20 @@ import express from "express";
 
 const app = express();
 
-app.get("/", function(request, response){
-  // console.log(request);
-  // console.log(response);
-  response.send("<h1>Planet of Jay</h1>");
+app.get("/", function(req, res){
+  res.send("<h1>Planet of Jay</h1>");
 });
 
-app.get("/contact", function(request, response){
-  response.send("Contact me at: jae44dv@gmail.com");
+app.get("/contact", function(req, res){
+  res.send("Contact me at: jae44dv@gmail.com");
+});
+
+app.get("/about", function(req, res){
+  res.send("Here is Jaejin Lee. I love code.");
+});
+
+app.get("/hobbies", function(req, res){
+  res.send("<ul><li>Coffee</li><li>Beer</li><li>Code</li></ul>");
 });
 
 app.listen(3000, function(){
