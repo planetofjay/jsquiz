a84c0e3f57a5933f947339e6f655f6d03d1d7d26
PlanetofJay <jae44dv@gmail.com>
2023-06-13 16:51:27 -0400

    test url,path

--- playground/node/Express/calculator/calculator.js
+++ playground/node/Express/calculator/calculator.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.get("/", function(req,res){
-  res.send("<h1>Hello World</h1>");
+  console.log(__dirname);
 });
 
 app.listen(3000, function(req,res){
-  console.log("Server started on port 3000");
+  console.log("Server is running on port 3000");
 });
