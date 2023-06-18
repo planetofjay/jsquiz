0daf1c124c287115673a338508f44f3d534b5bd1
PlanetofJay <jae44dv@gmail.com>
2023-06-18 16:18:07 -0400

    update

--- playground/node/Express/weather/weather.js
+++ playground/node/Express/weather/weather.js
@@ -9,38 +9,36 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/weather.html");
 });
 
-app.post("/", function(req,res){
-  console.log(req.body.cityName);
+app.post("/", function (req, res) {
+  const city = req.body.cityName;
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=${unit}`;
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const desc = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write("<p>The weather is currently " + "<b>" + desc + "</b>" + ".</p>");
+      res.write("<h1>The temperature in " + city + " is " + temp + " degrees Celcius.</h1>");
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    })
+  });
 });
 
-// const city = "Toronto";
-// const unit = "metric";
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=${unit}`;
 
-// https.get(url, function(response){
-//   console.log(response.statusCode);
-
-//   response.on("data", function(data){
-//     const weatherData = JSON.parse(data);
-//     const temp = weatherData.main.temp;
-//     const desc = weatherData.weather[0].description;
-//     const icon = weatherData.weather[0].icon;
-//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-//     res.write("<p>The weather is currently " + "<b>" + desc + "</b>" + ".</p>");
-//     res.write("<h1>The temperature in Toronto is " + temp + " degrees Celcius.</h1>");
-//     res.write("<img src=" + imageURL + ">");
-//     res.send();
-//   })
-// });
-
-
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
