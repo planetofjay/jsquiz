48320cd397ca00b027261fb6ce946136347c0c11
PlanetofJay <jae44dv@gmail.com>
2023-06-18 16:10:49 -0400

    post req

--- playground/node/Express/weather/weather.js
+++ playground/node/Express/weather/weather.js
@@ -1,31 +1,44 @@
 import express from "express";
 import https from "https";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import bodyParser from "body-parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req,res){
-
-  const city = "Toronto";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
-  
-  https.get(url, function(response){
-    console.log(response.statusCode);
-
-    response.on("data", function(data){
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const desc = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-      res.write("<p>The weather is currently " + "<b>" + desc + "</b>" + ".</p>");
-      res.write("<h1>The temperature in Toronto is " + temp + " degrees Celcius.</h1>");
-      res.write("<img src=" + imageURL + ">");
-      res.send();
-    })
-  });
-
+  res.sendFile(__dirname + "/weather.html");
 });
+
+app.post("/", function(req,res){
+  console.log(req.body.cityName);
+});
+
+// const city = "Toronto";
+// const unit = "metric";
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=${unit}`;
+
+// https.get(url, function(response){
+//   console.log(response.statusCode);
+
+//   response.on("data", function(data){
+//     const weatherData = JSON.parse(data);
+//     const temp = weatherData.main.temp;
+//     const desc = weatherData.weather[0].description;
+//     const icon = weatherData.weather[0].icon;
+//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+//     res.write("<p>The weather is currently " + "<b>" + desc + "</b>" + ".</p>");
+//     res.write("<h1>The temperature in Toronto is " + temp + " degrees Celcius.</h1>");
+//     res.write("<img src=" + imageURL + ">");
+//     res.send();
+//   })
+// });
 
 
 app.listen(3000, function(){
