2b1da4a0fdc69c9b18ea871a6cd82c55a830908b
PlanetofJay <jae44dv@gmail.com>
2023-06-30 19:22:38 -0400

    generate mail api

--- playground/node/Express/newsletter/news.js
+++ playground/node/Express/newsletter/news.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import request from "request";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import https from "https";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -19,11 +20,35 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  let firstName = req.body.fName;
-  let lastName = req.body.lName;
-  let email = req.body.email;
-
-  console.log(firstName, lastName, email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = `https://us21.api.mailchimp.com/3.0/lists/${process.env.MAIL_UNIQUE_KEY}`;
+  const options = {
+    method: "POST",
+    auth: `Jay1:${process.env.MAIL_API_KEY}`
+  };
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+  
+  request.write(jsonData);
+  request.end();
 });
  
 app.listen(3000, function(){
