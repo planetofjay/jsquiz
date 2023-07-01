f39f8b5797cf79a79c48cf334914f0ff28e5c7cd
PlanetofJay <jae44dv@gmail.com>
2023-07-01 16:18:16 -0400

    test dotenv/config

--- playground/node/Express/newsletter/news.js
+++ playground/node/Express/newsletter/news.js
@@ -7,6 +7,7 @@ import request from "request";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import https from "https";
+import 'dotenv/config';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -23,6 +24,7 @@ app.post("/", function(req, res){
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
+
   const data = {
     members: [
       {
@@ -35,12 +37,15 @@ app.post("/", function(req, res){
       }
     ]
   };
+  
   const jsonData = JSON.stringify(data);
   const url = `https://us21.api.mailchimp.com/3.0/lists/${process.env.MAIL_UNIQUE_KEY}`;
+
   const options = {
     method: "POST",
     auth: `jay1:${process.env.MAIL_API_KEY}`
   };
+
   const request = https.request(url, options, function(response){
     response.on("data", function(data){
       console.log(JSON.parse(data));
