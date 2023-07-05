c99b22811430466fbc9eebb11150629edf8f3206
PlanetofJay <jae44dv@gmail.com>
2023-07-05 17:14:02 -0400

    test EJS

--- playground/ejs/todolist-v1/app.js
+++ playground/ejs/todolist-v1/app.js
@@ -9,19 +9,41 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.set('view engine', 'ejs');
+
 app.get("/", function (req, res) {
 
   let today = new Date();
   let currentDay = today.getDay();
   let day = "";
 
-  if (currentDay === 6 || currentDay === 0) {
-    day = "Weekend";
-    res.sendFile(__dirname + "/weekend.html");
-  } else {
-    day = "Weekday"
-    res.sendFile(__dirname + "/weekday.html");
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+    console.log("Error: current day is equal to: " + currentDay);
   }
+
+  res.render("list", { kindOfDay: day });
 
 });
 
